08627594252594b9fa949722a9e551cba65a83a7
iamabdil <51669829+iamabdil@users.noreply.github.com>
2022-01-23 07:48:31 +0000

    first commit

---
+++ docs/precache-manifest.c0924e9c295cf29ee3d7740916ea777b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aeef5cd84bc3892bb9d93336281f5dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4513518e4449bfe0bc10",
+    "url": "/static/css/main.a33486bb.chunk.css"
+  },
+  {
+    "revision": "418b0be5aa72231d3ab0",
+    "url": "/static/js/2.6d62e0f9.chunk.js"
+  },
+  {
+    "revision": "13e2fb31210046457243d3d2e26ef151",
+    "url": "/static/js/2.6d62e0f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4513518e4449bfe0bc10",
+    "url": "/static/js/main.4f2cd00c.chunk.js"
+  },
+  {
+    "revision": "f35af6f7ccead2fe5dbb",
+    "url": "/static/js/runtime-main.4422d9b2.js"
+  }
+]);
